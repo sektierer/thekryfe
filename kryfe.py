3bc25dc2693ffa557c3e094b148aca43247755f0
Sektierer⁴ <80164708+Sorami1@users.noreply.github.com>
2022-07-13 01:41:10 +0200

    Create kryfe.py

---
+++ kryfe.py
@@ -0,0 +1,19 @@
+import discord
+from random import choice
+from discord.ext import commands
+
+kryfe = commands.Bot(command_prefix = '@Толерантный 2', bot = True)
+
+@bot.event
+async def on_ready():
+  while True:
+    try:
+      question = await kryfe.wait_for(stupid_question, timeout = 9999)
+    except TimeoutExcept:
+      await voice.leave()
+    else:
+      list = [other.stupid.question, stupid.answer, ignore]
+      await question.reply(choice(list))
+        
+
+bot.run('kryfe.lox')
